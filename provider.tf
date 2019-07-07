7048aff4f35818ea0c3e012c9797bd8f5da35e3a
Hossein Salahi <salahi.hossein@gmail.com>
2019-07-07 18:36:52 +0200

    bugfix: incorrect vars

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "google" {
-  credentials = "${file("${var.shared_credentials_file}")}"
-  project     = "${var.shared_credentials_file}"
+  credentials = "${file(var.shared_credentials_file)}"
+  project     = "${var.project}"
   region      = "${var.region}"
   zone        = "${var.zone}"
 }
