ef1a308aad49024fcd6bcccb1e4d64f6c309b982
Hossein Salahi <salahi.hossein@gmail.com>
2020-05-03 20:43:52 +0200

    provision k8s nodes

---
+++ k8s_nodes.tf
@@ -0,0 +1,35 @@
+resource "google_compute_instance" "k8s_master" {
+  count        = var.master_count
+  name         = "${var.master_name}${count.index + 1}"
+  machine_type = var.k8s_machine_type
+
+  tags = ["k8s", "master", "controller"]
+
+  boot_disk {
+    initialize_params {
+      image = var.k8s_image_type
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.k8s-subnet.name
+  }
+}
+
+resource "google_compute_instance" "k8s_worker" {
+  count = var.worker_count
+  name  = "${var.worker_name}${count.index + 1}"
+  machine_type = var.k8s_machine_type
+
+  tags = ["k8s", "worker"]
+
+  boot_disk {
+    initialize_params {
+      image = var.k8s_image_type
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.k8s-subnet.name
+  }
+}
