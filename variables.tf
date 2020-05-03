505a18a5df8b4089e6d104e8e0bb139e406a7643
Hossein Salahi <salahi.hossein@gmail.com>
2020-05-03 20:49:37 +0200

    use variables

---
+++ variables.tf
@@ -0,0 +1,43 @@
+variable "subnet_cidr" {
+  default = "10.127.0.0/20"
+}
+
+variable "network_name" {
+  default = "labor"
+}
+
+variable "k8s_machine_type" {
+  type = string
+}
+
+variable "k8s_image_type" {
+  type = string
+}
+
+variable "lb_image_type" {
+  type = string
+}
+
+variable "lb_machine_type" {
+  type = string
+}
+
+variable "master_name" {
+  type = string
+}
+
+variable "worker_name" {
+  type = string
+}
+
+variable "lb_name" {
+  type = string
+}
+
+variable "master_count" {
+  default = 1
+}
+
+variable "worker_count" {
+  default = 1
+}
