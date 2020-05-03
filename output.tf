a77a32e2110d4f3a1b6e57cf1058103ffa3e74f0
Hossein Salahi <salahi.hossein@gmail.com>
2020-05-03 20:48:30 +0200

    show load balancer ip

--- output.tf
+++ output.tf
@@ -1,7 +1,11 @@
 output "master_ip" {
-  value = "${google_compute_instance.k8s_master.*.network_interface.0.access_config}"
+  value = "${google_compute_instance.k8s_master.*.network_interface.0.network_ip}"
 }
 
 output "worker_ip" {
-  value = "${google_compute_instance.k8s_worker.*.network_interface.0.access_config}"
+  value = "${google_compute_instance.k8s_worker.*.network_interface.0.network_ip}"
+}
+
+output "lb_ip" {
+  value = "${google_compute_instance.k8s_loadbalancer.*.network_interface.0.network_ip}"
 }
