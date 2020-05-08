a3a5a34144f929b324ce968cd98b896c420c44bc
Hossein Salahi <h.salahi@reply.de>
2020-05-08 17:30:53 +0200

    added gcp instnace name to ansible inventory

--- inventory.tf
+++ inventory.tf
@@ -3,25 +3,20 @@
 *
 */
 data "template_file" "inventory" {
-  template = "${file("${path.module}/templates/inventory.tpl")}"
+  template = file("${path.module}/templates/inventory.tpl")
 
   vars = {
-    connection_strings_master  = join("\n", formatlist("%s ansible_host=%s", google_compute_instance.k8s_master.*.name, google_compute_instance.k8s_master.*.network_interface.0.network_ip))
-    #connection_strings_master = 
-    #connection_strings_node   = "${join("\n", format("%s ansible_host=%s", google_compute_instance.k8s_worker.*.network_interface.0.network_ip))}"
-    #connection_strings_etcd   = "${join("\n", format("%s ansible_host=%s", google_compute_instance.k8s_master.*.network_interface.0.network_ip))}"
-    #list_master               = "${join("\n", google_compute_instance.k8s_master.*.network_ip)}"
-    #list_node                 = "${join("\n", google_compute_instance.k8s_master.*.network_ip)}"
-    #list_etcd                 = "${join("\n", google_compute_instance.k8s_master.*.network_ip)}"
+    connection_strings_master = join("\n", formatlist("%s ansible_host=%s", google_compute_instance.k8s_master.*.name, google_compute_instance.k8s_master.*.network_interface.0.network_ip))
+    connection_strings_node   = join("\n", formatlist("%s ansible_host=%s", google_compute_instance.k8s_master.*.name, google_compute_instance.k8s_master.*.network_interface.0.network_ip))
+    connection_strings_etcd   = join("\n", formatlist("%s ansible_host=%s", google_compute_instance.k8s_master.*.name, google_compute_instance.k8s_master.*.network_interface.0.network_ip))
+    list_master               = join("\n", google_compute_instance.k8s_master.*.name)
+    list_node                 = join("\n", google_compute_instance.k8s_worker.*.name)
+    list_etcd                 = join("\n", google_compute_instance.k8s_master.*.name)
   }
 }
 
-resource "null_resource" "inventories" {
-  provisioner "local-exec" {
-    command = "echo '${data.template_file.inventory.rendered}' > ${var.inventory_file}"
-  }
-
-  triggers = {
-    template = "${data.template_file.inventory.rendered}"
-  }
+resource "google_storage_bucket_object" "inventory" {
+  name    = "hosts"
+  content = data.template_file.inventory.rendered
+  bucket  = var.bucket
 }
