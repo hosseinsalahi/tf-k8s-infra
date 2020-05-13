95fa008bc757f3b0d0dc2d3228c1110857767812
Hossein Salahi <h.salahi@reply.de>
2020-05-13 09:16:53 +0000

    bastion and inventory

--- output.tf
+++ output.tf
@@ -6,8 +6,8 @@ output "worker_ip" {
   value = google_compute_instance.k8s_worker.*.network_interface.0.network_ip
 }
 
-output "lb_ip" {
-  value = google_compute_instance.k8s_loadbalancer.network_interface.0.access_config.0.nat_ip
+output "bastion_ip" {
+  value = google_compute_instance.k8s_bastion.network_interface.0.access_config.0.nat_ip
 }
 
 output "hosts" {
